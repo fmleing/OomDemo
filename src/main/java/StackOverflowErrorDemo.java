83c30a63b359268c9c465c09deb454557ec59e41
Mengleifeng <fmleing@163.com>
2019-12-31 16:15:24 +0800

    StackOverflowError例子

---
+++ src/main/java/StackOverflowErrorDemo.java
@@ -0,0 +1,10 @@
+public class StackOverflowErrorDemo {
+
+    public static void main(String[] args) {
+        stackOverflow();
+    }
+
+    public static void stackOverflow(){
+        stackOverflow();
+    }
+}
