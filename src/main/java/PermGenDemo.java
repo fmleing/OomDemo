da533895016352339fb385169f3ead529344dad0
Mengleifeng <fmleing@163.com>
2020-01-03 15:09:34 +0800

    OOM - premGen sapce 错误

---
+++ src/main/java/PermGenDemo.java
@@ -0,0 +1,16 @@
+
+
+public class PermGenDemo {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100000000; i++) {
+
+
+        }
+    }
+
+    //public static Class generate(String name) {
+    //    ClassPool pool = ClassPool.getDefault();
+    //    return pool.makeClass(name).toClass;
+    //}
+}
